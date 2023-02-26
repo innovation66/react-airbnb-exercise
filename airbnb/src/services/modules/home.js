a59aa90c24d496137a28dff335f61971fac8674b
lzs <2992819638@qq.com>
2023-02-26 21:53:04 +0800

    03

--- airbnb/src/services/modules/home.js
+++ airbnb/src/services/modules/home.js
@@ -5,3 +5,30 @@ export function getHomeGoodPriceData() {
         url: "/home/goodprice"
     })
 }
+export function getHomeHighScoreData() {
+    return lzsRequest.get({
+      url: "/home/highscore"
+    })
+}
+export function getHomeDiscountData() {
+    return lzsRequest.get({
+        url: "/home/discount"        
+    })
+}
+export function getHomeHotRecommendData() {
+    return lzsRequest.get({
+        url: "/home/hotrecommenddest"
+    })
+}
+export function getHomeLongforData() {
+    return lzsRequest.get({
+        url: "/home/longfor"       
+    })
+}
+
+export function getHomePlusData() {
+    return lzsRequest.get({
+      url: "/home/plus"
+    })
+  }
+  
--- airbnb/src/views/home/style.js
+++ airbnb/src/views/home/style.js
@@ -5,9 +5,8 @@ export const HomeWrapper = styled.div`
   > .content {
     width: 1032px;
     margin: 0 auto;
+    
   }
 
-  .good-price {
-    margin-top: 30px;
-  }
+ 
 `
